b8fb2bb4801cc2c907f18f99aa56480b43a37c3e
powerlich227 <yemingxiao1990@gmail.com>
2021-02-22 21:58:45 -0600

    update

--- LowestCommonAncestorOfABinarySearchTree.java
+++ LowestCommonAncestorOfABinarySearchTree.java
@@ -3,8 +3,15 @@ public class LowestCommonAncestorOfABinarySearchTree {
 	public static void main(String[] args) {
 		System.out.println();
 	}
-	public TreeNode lowestCommonAncestor() {
-		
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+		if (root == null)
+			return null;		
+		if (root.val > Math.max(p.val, q.val))
+			return lowestCommonAncestor(root.left, p, q);
+		else if (root.val < Math.min(p.val, q.val))
+			return lowestCommonAncestor(root.right, p, q);
+		else
+			return root;
 	}
 
 }
