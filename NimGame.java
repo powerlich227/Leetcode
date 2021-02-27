5a175c40517926d77da5a3e0dcabf24ce49177ba
powerlich227 <yemingxiao1990@gmail.com>
2021-02-26 22:49:07 -0600

    update

---
+++ NimGame.java
@@ -0,0 +1,10 @@
+
+public class NimGame {
+	public void main(String[] args) {
+		System.out.println();
+	}
+	// W W W L W W W L W W W L 
+	public boolean canWinNim(int n) {
+		return n % 4 != 0;
+	}
+}
