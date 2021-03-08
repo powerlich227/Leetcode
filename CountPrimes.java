3860857d0be18556a9e39db58dfe260244bcc608
powerlich227 <yemingxiao1990@gmail.com>
2021-03-08 10:55:18 -0600

    sieve of Eratosthenes

--- CountPrimes.java
+++ CountPrimes.java
@@ -3,10 +3,21 @@ public class CountPrimes {
 	public static void main(String[] args) {
 		System.out.println();
 	}
+	
+	/*
+	 * Sieve of Eratosthenes
+	 * 2,4,6...
+	 * 3,6,9...
+	 * ...
+	 * n^0.5, 2*n^0.5, ....
+	 * 
+	 * Count the number of prime numbers less than a non-negative number n
+	 */
+	
 	public int countPrimes(int n) {
 		int res = 0;
-		boolean[] prime = new boolean[n];
-		for (int i = 2; i < n; i++) {
+		boolean[] prime = new boolean[n]; // 1...n-1 "false"
+		for (int i = 2; i < n; i++) { // start from 2
 			if (prime[i])
 				continue;
 			res++;
