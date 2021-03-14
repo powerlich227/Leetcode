e834c2e127dbfa20f59331c7b68f320ba0ca1a3c
powerlich227 <yemingxiao1990@gmail.com>
2021-03-14 18:54:12 -0500

    Medium, DP + HashSet

---
+++ WordBreak.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class WordBreak {
+	public static void main(String[] args) {
+		System.out.println();
+	}
+	/*
+	 * dp
+	 * hashset
+	 */
+	public boolean wordBreak(String s, List<String> wordDict) {
+		Set<String> set = new HashSet<>();
+		for (String word: wordDict)
+			set.add(word);
+		int n = s.length();
+		boolean[] dp = new boolean[n + 1];
+		dp[0] = true;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 0; j < i; j++) {
+				if (dp[j] == true && set.contains(s.substring(j, i))) {
+					dp[i] = true;
+					break;
+				}
+			}
+		}
+		return dp[n];
+	}
+}
