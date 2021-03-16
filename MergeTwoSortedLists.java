312490bf1fcd7c2eeab930d78d9f6981be6b8c98
powerlich227 <yemingxiao1990@gmail.com>
2021-03-16 16:19:29 -0500

    Easy, Recursion

--- MergeTwoSortedLists.java
+++ MergeTwoSortedLists.java
@@ -5,24 +5,48 @@ public class MergeTwoSortedLists {
 	}
 	
 	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-		ListNode node = new ListNode(0);
-		ListNode temp = node;
+		ListNode dummy = new ListNode(-1);
+		ListNode cur = dummy;
 		while (l1 != null && l2 != null) {
 			if (l1.val <= l2.val) {
-				temp.next = l1;
+				cur.next = l1;
 				l1 = l1.next;
 			}
 			else {
-				temp.next = l2;
+				cur.next = l2;
 				l2 = l2.next;
 			}
-			temp = temp.next;
+			cur = cur.next;
 		}
 		if (l1 != null)
-			temp.next = l1;
+			cur.next = l1;
 		else
-			temp.next = l2;
+			cur.next = l2;
 		
-		return node.next;
+		return dummy.next;
+	}
+	
+	// Recursion
+	public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+//		if (l1 == null) 
+//			return l2;
+//		if (l2 == null)
+//			return l1;
+//		if (l1.val < l2.val) {
+//			l1.next = mergeTwoLists2(l1.next, l2);
+//			return l1;
+//		}
+//		else {
+//			l2.next = MergeTwoLists2(l1, l2.next);
+//			return l2;
+//		}
+		if (l1 == null || (l2 != null && l2.val <= l1.val)) {
+			ListNode temp = l1;
+			l1 = l2;
+			l2 = temp;
+		}
+		if (l1 != null)
+			l1.next = mergeTwoLists2(l1.next, l2);
+		return l1;
 	}
 }
