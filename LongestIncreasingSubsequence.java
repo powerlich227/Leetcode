fa2e1c423c446afe7e4febb6da31b63b641755b2
powerlich227 <yemingxiao1990@gmail.com>
2021-04-13 20:38:24 -0500

    Medium, DP+BinarySearch

---
+++ LongestIncreasingSubsequence.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+
+/*
+ * LeetCode 300
+ * 
+ * Longest Increasing Subsequence
+ * 
+Given an integer array nums, return the length of the longest strictly increasing subsequence.
+A subsequence is a sequence that can be derived from an array by deleting some or no elements without changing the order of the remaining elements. 
+For example, [3,6,2,7] is a subsequence of the array [0,3,1,6,2,2,7].
+
+Example 1:
+Input: nums = [10,9,2,5,3,7,101,18]
+Output: 4
+Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+
+Example 2:
+Input: nums = [0,1,0,3,2,3]
+Output: 4
+
+Example 3:
+Input: nums = [7,7,7,7,7,7,7]
+Output: 1
+
+ */
+public class LongestIncreasingSubsequence {
+	// dp + binarySearch
+	// return length of "non-zero" dp array
+	public static int lengthOfLIS(int[] nums) {
+		int[] dp = new int[nums.length];
+		int res = 0;
+		for (int num : nums) {
+			int i = Arrays.binarySearch(dp, 0, res, num); 
+			if (i < 0) // i: the index of the first element in the range greater than "num", or "res" if all elements less than "num".
+				i = -(i + 1); // return (-i - 1)
+			dp[i] = num;
+			if (i == res)
+				res++;
+		}
+		return res;
+	}
+	
+	public static void main(String[] args) {
+		int[] nums = {10, 9, 2, 5, 3, 7, 101, 18};
+		System.out.println(lengthOfLIS(nums));
+	}
+
+}
