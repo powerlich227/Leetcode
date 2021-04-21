c2f63c73f3d4d8333d7de2d3cc9bf155447da647
powerlich227 <yemingxiao1990@gmail.com>
2021-04-20 21:00:39 -0500

    HashTable+SlidingWindow

--- MinimumWindowSubstring.java
+++ MinimumWindowSubstring.java
@@ -1,36 +1,68 @@
 import java.util.HashMap;
+/*
+ * LeetCode 76 Hard
+ * 
+ * Minimum Window Substring
+ * 
+Given two strings s and t, return the minimum window in s which will contain all the characters in t. If there is no such window in s that covers all characters in t, return the empty string "".
 
+Note that If there is such a window, it is guaranteed that there will always be only one unique minimum window in s.
+
+Example 1:
+Input: s = "ADOBECODEBANC", t = "ABC"
+Output: "BANC"
+
+Example 2:
+Input: s = "a", t = "a"
+Output: "a"
+ 
+Constraints:
+
+1 <= s.length, t.length <= 105
+s and t consist of English letters.
+
+Follow up: Could you find an algorithm that runs in O(n) time?
+ */
 public class MinimumWindowSubstring {
 	public static void main(String[] args) {
-		System.out.println();
+		String s = "ADOBECODEBANC", t = "ABC";
+		System.out.println(minWindow(s, t));
 	}
-	public String minWindow(String s, String t) {
+	public static String minWindow(String s, String t) {
 		String res = "";
 		int m = s.length(), n = t.length();
-		HashMap<Character, Integer> hashMap = new HashMap<Character, Integer>();		
+		HashMap<Character, Integer> hash = new HashMap<>();
+		
 		for (int i = 0; i < n; i++) {
-			hashMap.put(t.charAt(i), hashMap.getOrDefault(t.charAt(i), 0) + 1);
+			char c = t.charAt(i);
+			hash.put(c, hash.getOrDefault(c, 0) + 1);
 		}
-		int l = 0, r = 0, count = 0, width = Integer.MAX_VALUE;
+		
+		int l = 0, r = 0, width = Integer.MAX_VALUE;
 		while (r < m) {
-			if (hashMap.containsKey(s.charAt(r))) {
-				hashMap.put(s.charAt(r), hashMap.get(s.charAt(r)) - 1);
-				if (hashMap.get(s.charAt(r)) >= 0)
-					count++;                
+			char c = s.charAt(r);
+			r++;
+			
+			if (hash.containsKey(c)) {
+				hash.put(c, hash.get(c) - 1);
+				if (hash.get(c) >= 0)
+					n--;                
 			}
-			while (count == t.length()) {
+			while (n == 0) {
 				if (width > r - l + 1) {
 					width = r - l + 1;
-					res = (r == m - 1) ? s.substring(l) : s.substring(l, r + 1);
+					res = s.substring(l, r);
 				}
-				if (hashMap.containsKey(s.charAt(l)) ) {
-					hashMap.put(s.charAt(l), hashMap.get(s.charAt(l)) + 1);
-					if (hashMap.get(s.charAt(l)) > 0)
-						count--;
-				}
+				
+				char c2 = s.charAt(l);
 				l++;
+
+				if (hash.containsKey(c2) ) {
+					hash.put(c2, hash.get(c2) + 1);
+					if (hash.get(c2) >= 1)
+						n++;
+				}
 			}
-			r++;
 		}
 		return res;
 	}
