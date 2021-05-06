47ea831b4b59d09ac82c3ce945461eca28463e0f
powerlich227 <yemingxiao1990@gmail.com>
2021-05-06 16:27:59 -0500

    Easy, BinarySearch

---
+++ BinarySearch.java
@@ -0,0 +1,54 @@
+/*
+ * LeetCode 702 Easy
+ * 
+ * Binary Search
+ * 
+Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. 
+If target exists, then return its index. Otherwise, return -1.
+
+ 
+Example 1:
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+
+Example 2:
+Input: nums = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in nums so return -1
+ 
+
+Constraints:
+1 <= nums.length <= 10^4
+-9999 <= nums[i], target <= 9999
+All the integers in nums are unique.
+nums is sorted in an ascending order.
+
+ */
+public class BinarySearch {
+	public int search(int[] nums, int target) {
+		int left = 0, right = nums.length;
+		
+		while (left < right) {
+			int mid = left + (right - left) / 2;
+			if (nums[mid] == target) {
+				return mid;
+			}
+			else if (nums[mid] < target) {
+				left = mid + 1;
+			}
+			else if (nums[mid] > target) {
+				right = mid;
+			}
+		}
+		return -1;
+	}
+	public static void main(String[] args) {
+		int[] nums = {-1, 0, 3, 5, 9, 12};
+		int target = 9, target2 = 2;
+		BinarySearch test = new BinarySearch();
+		System.out.println(test.search(nums, target));
+		System.out.println(test.search(nums, target2));
+	}
+
+}
