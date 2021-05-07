225893771dadaeb1cf71d142281190fa57c6b8fa
powerlich227 <yemingxiao1990@gmail.com>
2021-05-07 11:59:30 -0500

    Easy, TwoPointers/BinarySearch

--- TwoSumII.java
+++ TwoSumII.java
@@ -1,23 +1,52 @@
+/*
+ * LeetCode 167 Easy
+ * 
+ * Two Sum II - Input Array is sorted
+ * 
+Given an array of integers numbers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+Return the indices of the two numbers (1-indexed) as an integer array answer of size 2, where 1 <= answer[0] < answer[1] <= numbers.length.
+You may assume that each input would have exactly one solution and you may not use the same element twice.
 
+ 
+Example 1:
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
+
+Example 2:
+Input: numbers = [2,3,4], target = 6
+Output: [1,3]
+
+Example 3:
+Input: numbers = [-1,0], target = -1
+Output: [1,2]
+ 
+
+Constraints:
+2 <= numbers.length <= 3 * 10^4
+-1000 <= numbers[i] <= 1000
+numbers is sorted in increasing order.
+-1000 <= target <= 1000
+Only one valid answer exists.
+ */
 public class TwoSumII {
 	public static void main(String[] args) {
 		System.out.println();
 	}
+	// two pointers or binary search
 	public int[] twoSum(int[] numbers, int target) {
 		int left = 0;
 		int right = numbers.length - 1;
 
 		while (left < right) {
 			int sum = numbers[left] + numbers[right];
-			if (sum == target) {
-				int[] res = new int[]{left + 1, right + 1};
-				return res;
-			}
+			if (sum == target)
+				return new int[]{left + 1, right + 1};
 			else if (sum < target)
 				left++;
-			else
+			else if (sum > target)
 				right--;
 		}
-		return null;
+		return new int[] {-1, -1};
 	}
 }
