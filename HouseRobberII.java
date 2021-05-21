074c6198fd4bff7f2429aeab8c85ed975b2a5c8b
powerlich227 <yemingxiao1990@gmail.com>
2021-05-20 22:46:13 -0500

    Medium, DP

--- HouseRobberII.java
+++ HouseRobberII.java
@@ -40,7 +40,6 @@ public class HouseRobberII {
 	}
 	// dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i])
 	public int robRange(int[] nums, int start, int end) {
-		int n = nums.length;
 		int dp_i_2 = 0, dp_i_1 = 0, dp_i = 0;
 		
 		for (int i = start; i < end; i++) {
