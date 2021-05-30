a10aefbd43deba343516a5c9809ca7135c7dd283
powerlich227 <yemingxiao1990@gmail.com>
2021-05-30 17:41:42 -0500

    Medium, sort

--- GroupAnagrams.java
+++ GroupAnagrams.java
@@ -1,9 +1,39 @@
 import java.util.*;
+/*
+ * LeetCode 49 Medium
+ * 
+ * Group Anagrams
+ * 
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+Example 1:
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Example 2:
+Input: strs = [""]
+Output: [[""]]
+
+Example 3:
+Input: strs = ["a"]
+Output: [["a"]]
+ 
+
+Constraints:
+1 <= strs.length <= 10^4
+0 <= strs[i].length <= 100
+strs[i] consists of lower-case English letters.
+ */
 public class GroupAnagrams {
 	public static void main(String[] args) {
-		String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
-		System.out.println(groupAnagrams2(strs));
+		String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"}, strs2 = {""}, strs3 = {"a"};
+		System.out.println(groupAnagrams(strs));
+		System.out.println(groupAnagrams(strs2));
+		System.out.println(groupAnagrams(strs3));
 	}
+	// sort
 	public static List<List<String>> groupAnagrams(String[] strs) {
 		Map<String, List<String>> map = new HashMap<>();
 		for (String str : strs) {
@@ -11,24 +41,6 @@ public class GroupAnagrams {
 			Arrays.sort(ch);
 			String key = String.valueOf(ch);
 			if (!map.containsKey(key)) 
-				map.put(key, new ArrayList<>());
-			map.get(key).add(str);
-		}
-		return new ArrayList<>(map.values());
-	}
-	public static List<List<String>> groupAnagrams2(String[] strs) {
-		Map<String, List<String>> map = new HashMap<>();
-		int[] count = new int[26];
-		for (String str : strs) {
-			Arrays.fill(count, 0);
-			char[] ch = str.toCharArray();
-			for (char c : ch)
-				count[c - 'a']++;
-			StringBuilder sb = new StringBuilder();
-			for (int i = 0; i < 26; i++)
-				sb.append(count[i]);
-			String key = sb.toString();
-			if (!map.containsKey(key))
 				map.put(key, new ArrayList<>());
 			map.get(key).add(str);
 		}
