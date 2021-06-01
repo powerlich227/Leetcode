a2b50dec83680172d0455725a25d41b1943edd7a
powerlich227 <yemingxiao1990@gmail.com>
2021-05-31 23:40:43 -0500

    Medium, QucikSort-Pivot

--- KthLargestElementInAnArray.java
+++ KthLargestElementInAnArray.java
@@ -1,20 +1,47 @@
 import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.Queue;
+/*
+ * LeetCode 215 Medium
+ * 
+ * Kth Largest Element in an Array
+ * 
+Given an integer array nums and an integer k, return the kth largest element in the array.
+Note that it is the kth largest element in the sorted order, not the kth distinct element.
 
+ 
+Example 1:
+Input: nums = [3,2,1,5,6,4], k = 2
+Output: 5
+
+Example 2:
+Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+Output: 4
+ 
+
+Constraints:
+1 <= k <= nums.length <= 10^4
+-104 <= nums[i] <= 10^4
+
+ */
 public class KthLargestElementInAnArray {
 	public static void main(String[] args) {
-		System.out.println();
+		KthLargestElementInAnArray test = new KthLargestElementInAnArray();
+		int[] nums = {3, 2, 1, 5, 6, 4}, nums2 = {3, 2, 3, 1, 2, 4, 5, 5, 6};
+		System.out.println(test.findKthLargest3(nums, 2));
+		System.out.println(test.findKthLargest3(nums2, 4));
 	}
 	/*
 	 * Arrays.sort
+	 * O(NlogN), O(1)
 	 */
 	public int findKthLargest(int[] nums, int k) {
 		Arrays.sort(nums);
-		return (nums.length - k - 1);
+		return nums[nums.length - k];
 	}
 	/*
 	 * PQ
+	 * O(Nlogk), O(N)
 	 */
 	public int findKthLargest2(int[] nums, int k) {
 		Queue<Integer> pq = new PriorityQueue<>();
@@ -27,6 +54,7 @@ public class KthLargestElementInAnArray {
 	}
 	/*
 	 * Quicksort
+	 * O(N), O(1)
 	 */
 	public int findKthLargest3(int[] nums, int k) {
 		int n = nums.length, l = 0, r = n - 1;
@@ -34,7 +62,7 @@ public class KthLargestElementInAnArray {
 			int pos = helper(nums, l, r);
 			if (pos == n - k)
 				return nums[pos];
-			if (pos > n - k)
+			else if (pos < n - k)
 				l = pos + 1;
 			else 
 				r = pos - 1;
