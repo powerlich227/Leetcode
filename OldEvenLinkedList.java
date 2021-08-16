c85707e39a9d801b5e07df85e6f9e437128a4db2
powerlich227 <yemingxiao1990@gmail.com>
2021-08-15 23:58:56 -0500

    Medium, LinkedList

---
+++ OldEvenLinkedList.java
@@ -0,0 +1,48 @@
+/*
+ * LeetCode 328 Medium
+ * 
+ * Odd Even Linked List
+ * 
+Given the head of a singly linked list, group all the nodes with odd indices together followed by the nodes with even indices, 
+and return the reordered list.
+
+The first node is considered odd, and the second node is even, and so on.
+Note that the relative order inside both the even and odd groups should remain as it was in the input.
+
+You must solve the problem in O(1) extra space complexity and O(n) time complexity.
+
+Example 1:
+Input: head = [1,2,3,4,5]
+Output: [1,3,5,2,4]
+
+Example 2:
+Input: head = [2,1,3,5,6,4,7]
+Output: [2,3,6,7,1,5,4]
+
+Constraints:
+n == number of nodes in the linked list
+0 <= n <= 10^4
+-10^6 <= Node.val <= 10^6
+ */
+public class OldEvenLinkedList {
+	public ListNode oddEvenList(ListNode head) {
+        if (head == null)
+            return null;
+        if (head.next == null)
+            return head;
+        ListNode odd = head, even = head.next;
+        ListNode temp = even;
+        
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = temp;
+        return head;
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+}
