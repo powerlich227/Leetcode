3fba83134b9739e629b189822e86f458754253c5
powerlich227 <yemingxiao1990@gmail.com>
2021-08-17 02:06:56 -0500

    Medium, LinkedList/Iteration/Recursion

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -1,4 +1,32 @@
+/*
+ * LeetCode 2 Medium
+ * 
+ * Add Two Numbers
+ * 
+You are given two non-empty linked lists representing two non-negative integers. 
+The digits are stored in reverse order, and each of their nodes contains a single digit. 
+Add the two numbers and return the sum as a linked list.
 
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example 1:
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+
+Example 2:
+Input: l1 = [0], l2 = [0]
+Output: [0]
+
+Example 3:
+Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+Output: [8,9,9,9,0,0,0,1]
+ 
+Constraints:
+The number of nodes in each linked list is in the range [1, 100].
+0 <= Node.val <= 9
+It is guaranteed that the list represents a number that does not have leading zeros.
+ */
 public class AddTwoNumbers {
 	public static void main(String[] args) {
 		System.out.println();
@@ -8,15 +36,10 @@ public class AddTwoNumbers {
         ListNode cur = dummy;
         int carry = 0;
         while (l1 != null || l2 != null) {
-//        	int digit1;
-//        	if (l1 == null)
-//        		digit1 = 0;
-//        	else
-//        		digit1 = l1.val;
-        	int digit1 = l1 == null ? 0 : l1.val;
-        	int digit2 = l2 == null ? 0 : l2.val;
+        	int digit1 = (l1 == null) ? 0 : l1.val;
+        	int digit2 = (l2 == null) ? 0 : l2.val;
         	int sum = digit1 + digit2 + carry;
-        	carry = sum >= 10 ? 1 : 0;
+        	carry = sum / 10;
         	cur.next = new ListNode(sum % 10);
         	cur = cur.next;
         	if (l1 != null)
