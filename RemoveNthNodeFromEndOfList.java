9222f327b60429e5266e58c960d5fcaf9e04579c
powerlich227 <yemingxiao1990@gmail.com>
2021-08-17 02:28:05 -0500

    Medium, LinkedList/TwoPointers

--- RemoveNthNodeFromEndOfList.java
+++ RemoveNthNodeFromEndOfList.java
@@ -1,4 +1,30 @@
+/*
+ * LeetCode 19 Meidum
+ * 
+ * Remove Nth Node From End of List
+ * 
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
 
+Example 1:
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+
+Example 2:
+Input: head = [1], n = 1
+Output: []
+
+Example 3:
+Input: head = [1,2], n = 1
+Output: [1]
+ 
+Constraints:
+The number of nodes in the list is sz.
+1 <= sz <= 30
+0 <= Node.val <= 100
+1 <= n <= sz
+ 
+Follow up: Could you do this in one pass?
+ */
 public class RemoveNthNodeFromEndOfList {
 	public static void main(String[] args) {
 		ListNode a = new ListNode(1);
@@ -10,11 +36,8 @@ public class RemoveNthNodeFromEndOfList {
 		b.next = c;
 		c.next = d;
 		d.next = e;
-		
-//		int n = 5;
-//		ListNode res = removeNthFromEnd(a, n);
-//		System.out.println(res.val);
 	}
+	// Two Pointers
 	public ListNode removeNthFromEnd(ListNode head, int n) {
 		if (head == null || head.next == null)
 			return null;
@@ -32,43 +55,4 @@ public class RemoveNthNodeFromEndOfList {
 		System.out.println(pre);
 		return head;
 	}
-	// dummy node w/o checking if head node will be deleted
-	public ListNode removeNthFromEnd2(ListNode head, int n) {
-		if (head == null || head.next == null)
-			return null;
-		ListNode dummy = new ListNode(0);
-		dummy.next = head;
-		ListNode pre = dummy, cur = dummy;
-		for (int i = 0; i < n; i++)
-			cur = cur.next;
-		while (cur.next != null) {
-			pre = pre.next;
-			cur = cur.next;
-		}
-		pre.next = pre.next.next;
-		return dummy.next;
-	}
-	// not in one pass
-	public ListNode removeNthFromEnd3(ListNode head, int n) {
-		if (head == null || head.next == null)
-			return null;
-		// get ListNode's length
-		ListNode cur = head;
-		int length = 0;
-		while (cur != null) {
-			cur = cur.next;
-			length++;
-		}
-		length -= n;
-		ListNode dummy = new ListNode(0);
-		dummy.next = head;
-		cur = dummy;
-		while (length > 0) {
-			cur = cur.next;
-			length--;
-		}
-		cur.next = cur.next.next;
-		return dummy.next;
-	}
-
 }
