ab615256fa08a81c6f8c43895e30cd9d22e5b38a
powerlich227 <yemingxiao1990@gmail.com>
2021-08-17 19:49:14 -0500

    Medium, LinkedList/Iteration/Recursion

--- SwapNodesInPairs.java
+++ SwapNodesInPairs.java
@@ -1,24 +1,42 @@
+/*
+ * LeetCode 24 Medium
+ * 
+ * Swap Nodes in Pairs
+ * 
+Given a linked list, swap every two adjacent nodes and return its head. 
+You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
 
+Example 1:
+Input: head = [1,2,3,4]
+Output: [2,1,4,3]
+
+Example 2:
+Input: head = []
+Output: []
+
+Example 3:
+Input: head = [1]
+Output: [1]
+ 
+Constraints:
+
+The number of nodes in the list is in the range [0, 100].
+0 <= Node.val <= 100
+ */
 public class SwapNodesInPairs {
 	public static void main(String[] args) {
 		System.out.println();	
 	}
-	// head = 1-2-3-4
+	// Recursion
 	public ListNode swapPairs(ListNode head) {
-		
-		ListNode dummy = new ListNode(0);
-		ListNode pre = dummy;
-		
-		dummy.next = head; // pre = dummy = 0-1-2-3-4
-		while (pre.next != null && pre.next.next != null) {
-			ListNode temp = pre.next.next; // temp = 2-3-4; temp = 4
-			pre.next.next = temp.next; // pre = 0-1-3-4; pre = 1-3
-			temp.next = pre.next; // temp = 2-1-3-4; temp = 4-3
-			pre.next = temp; // pre = 0-2-1-3-4; pre = 1-4-3
-			pre = temp.next; // pre = 1-3-4; pre = 3;
-		}
-		return dummy.next;
+		if (head == null || head.next == null)
+			return null;
+		ListNode temp = head.next;
+		head.next = swapPairs(head.next.next);
+		temp.next = head;
+		return temp;
 	}
+	// Iteration
 	public ListNode swapPairs2(ListNode head) {
 		if (head == null || head.next == null)
 			return head;
