42cb50656fdf2a99f4b90bc415c1193a760d15fd
powerlich227 <yemingxiao1990@gmail.com>
2021-08-25 22:49:05 -0500

    Medium, Design/String/Array

--- ImplementTrie.java
+++ ImplementTrie.java
@@ -67,7 +67,7 @@ public class ImplementTrie {
 		root = new TrieNode();
 	}
 	/** Insert a word into the trie.
-	 * O(m) **/
+	 * O(m) / O(m) **/
 	public void insert(String word) {
 		TrieNode node = root;
 		for (int i = 0; i < word.length(); i++) {
@@ -90,13 +90,15 @@ public class ImplementTrie {
 		}
 		return node;
 	}
-	/** Returns if the word is in the trie. **/
+	/** Returns if the word is in the trie. 
+	 * O(m) / O(1) **/
 	public boolean search(String word) {
 		TrieNode node = searchPrefix(word);
 		return node != null && node.isEnd();
 	}
 	
-	/** Returns if there is any word in the trie that starts with the given prefix. **/
+	/** Returns if there is any word in the trie that starts with the given prefix. 
+	 * O(m) / O(1) **/
 	public boolean startsWith(String prefix) {
 		TrieNode node = searchPrefix(prefix);
 		return node != null;
