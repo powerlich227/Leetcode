c2f1fce8f568b9d9504d104a1953489b6e1f9aa2
powerlich227 <yemingxiao1990@gmail.com>
2021-11-05 16:27:00 -0400

    Easy, BinaryTree/DFS/Stack

--- BinaryTreePreorderTraversal.java
+++ BinaryTreePreorderTraversal.java
@@ -1,4 +1,41 @@
 import java.util.*;
+
+/*
+ * LeetCode 144 Easy
+ * 
+ * Binary Tree Preorder Traversal
+ * 
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+Example 1:
+Input: root = [1,null,2,3]
+Output: [1,2,3]
+
+Example 2:
+Input: root = []
+Output: []
+
+Example 3:
+Input: root = [1]
+Output: [1]
+
+Example 4:
+Input: root = [1,2]
+Output: [1,2]
+
+Example 5:
+Input: root = [1,null,2]
+Output: [1,2]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+ 
+
+Follow up: Recursive solution is trivial, could you do it iteratively?
+ */
 public class BinaryTreePreorderTraversal {
 	public static void main(String[] args) {
 		System.out.println();
@@ -13,12 +50,11 @@ public class BinaryTreePreorderTraversal {
 		if (root == null)
 			return;
 		res.add(root.val);
-		if (root.left != null)
-			helper(root.left, res);
-		if (root.right != null)
-			helper(root.right, res);
+		helper(root.left, res);
+		helper(root.right, res);
 	}
-	//iterative
+	// follow-up: iterative
+	// stack
 	public List<Integer> preorderTraversal2(TreeNode root) {
 		List<Integer> res = new ArrayList<>();
 		Stack<TreeNode> stack = new Stack<>();
