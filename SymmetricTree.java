d680e87ff4a5b8c16eb22b4312b271d24ae6a18c
powerlich227 <yemingxiao1990@gmail.com>
2021-11-07 11:19:15 -0500

    Easy, BinaryTree/DFS/BFS/Queue

--- SymmetricTree.java
+++ SymmetricTree.java
@@ -1,11 +1,31 @@
 import java.util.LinkedList;
 import java.util.Queue;
+/*
+ * LeetCode 101 Easy
+ * 
+ * Symmetric Tree
+ * 
+Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
 
+Example 1:
+Input: root = [1,2,2,3,4,4,3]
+Output: true
+
+Example 2:
+Input: root = [1,2,2,null,3,null,3]
+Output: false
+ 
+Constraints:
+The number of nodes in the tree is in the range [1, 1000].
+-100 <= Node.val <= 100
+ 
+Follow up: Could you solve it both recursively and iteratively?
+ */
 public class SymmetricTree {
 	public static void main(String[] args) {
 		System.out.println();
 	}
-	// recursion
+	// dfs: recursion
 	public boolean isSymmetric(TreeNode root) {
 		if (root == null)
 			return true;
@@ -20,7 +40,7 @@ public class SymmetricTree {
 			return false;
 		return helper(left.left, right.right) && helper(left.right, right.left);
 	}
-	// iteration
+	// bfs: iteration, queue
 	public boolean isSymmetric2(TreeNode root) {
 		if (root == null)
 			return true;
