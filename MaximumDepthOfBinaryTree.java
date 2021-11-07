a56ef605c878fa674d13d27f072433e1f9d5a64f
powerlich227 <yemingxiao1990@gmail.com>
2021-11-07 10:21:10 -0500

    Easy, BinaryTree/DFS/BFS/Queue

--- MaximumDepthOfBinaryTree.java
+++ MaximumDepthOfBinaryTree.java
@@ -1,17 +1,48 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
+/*
+ * LeetCode 104 Easy
+ * 
+ * Maximum Depth of Binary Tree
+ * 
+Given the root of a binary tree, return its maximum depth.
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Example 1:
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+
+Example 2:
+Input: root = [1,null,2]
+Output: 2
+
+Example 3:
+Input: root = []
+Output: 0
+
+Example 4:
+Input: root = [0]
+Output: 1
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 10^4].
+-100 <= Node.val <= 100
+ */
+
 public class MaximumDepthOfBinaryTree {
 	public static void main(String[] args) {
 		System.out.println();
 	}
-	//Recursion
+	// DFS: Recursion
 	public int maxDepth(TreeNode root) {
 		if (root == null)
 			return 0;
 		return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
 	}
-	//Traverse
+	// BFS: Traverse
 	public int maxDepth2(TreeNode root) {
 		int res = 0;
 		if (root == null)
