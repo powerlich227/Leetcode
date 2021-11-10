ed7421c83536d67a9e66ab21ed8fa16c2117e226
powerlich227 <yemingxiao1990@gmail.com>
2021-11-09 23:08:25 -0500

    Medium, Array/BinarySearch

--- FindMinimumInRotatedSortedArray.java
+++ FindMinimumInRotatedSortedArray.java
@@ -6,8 +6,9 @@
 Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
 [4,5,6,7,0,1,2] if it was rotated 4 times.
 [0,1,2,4,5,6,7] if it was rotated 7 times.
+
 Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
-Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+Given the sorted rotated array nums of "unique" elements, return the minimum element of this array.
 You must write an algorithm that runs in O(log n) time.
 
  
@@ -36,14 +37,17 @@ All the integers of nums are unique.
 nums is sorted and rotated between 1 and n times.
  */
 public class FindMinimumInRotatedSortedArray {
+	// BinarySearch
+	// [left, right)
+	// left==right break
 	public int findMin(int[] nums) {
 		int left = 0, right = nums.length - 1;
 		while (left < right) {
 			int mid = left + (right - left) / 2;
-			if (nums[mid] > nums[right])
-				left = mid + 1;
-			else
+			if (nums[mid] < nums[right])
 				right = mid;
+			else if (nums[mid] > nums[right])
+				left = mid + 1;
 		}
 		return nums[left];
 	}
@@ -54,5 +58,4 @@ public class FindMinimumInRotatedSortedArray {
 		System.out.println(test.findMin(nums2));
 		System.out.println(test.findMin(nums3));
 	}
-
 }
