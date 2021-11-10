b1c5c7ab2f556dd71092eaaa3baefe3c53422af2
powerlich227 <yemingxiao1990@gmail.com>
2021-11-10 17:14:04 -0500

    Easy, Array/TwoPointers

--- MoveZeroes.java
+++ MoveZeroes.java
@@ -1,16 +1,51 @@
+import java.util.Arrays;
 
+/*
+ * LeetCode 283 Easy
+ * 
+ * Move Zeroes
+ * 
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+Note that you must do this in-place without making a copy of the array.
+
+ 
+Example 1:
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+Example 2:
+Input: nums = [0]
+Output: [0]
+ 
+Constraints:
+
+1 <= nums.length <= 10^4
+-2^31 <= nums[i] <= 2^31 - 1
+ 
+
+Follow up: Could you minimize the total number of operations done?
+ */
 public class MoveZeroes {
 	public static void main(String[] args) {
-		System.out.println();
+		MoveZeroes test = new MoveZeroes();
+		int[] nums = new int[] {0, 1, 0, 3, 12};
+		test.moveZeroes(nums);
+		System.out.println(Arrays.toString(nums));
 	}
-	public void movezeroes(int[] nums) {
-		for (int i = 0, j = 0, temp = 0; i < nums.length; i++) {
-			if (nums[i] != 0) {
-				temp = nums[j];
-				nums[j] = nums[i];
-				nums[i] = temp;
-				j++;
-			}		
+	// Two Pointers: slow & fast
+	public void moveZeroes(int[] nums) {
+		int slow = 0, fast = 0;
+		while (fast < nums.length) {
+			if (nums[fast] != 0) {
+                if (slow != fast) {
+                	// swap
+                    int temp = nums[fast];
+                    nums[fast] = nums[slow];
+                    nums[slow] = temp;
+                }
+				slow++;
+			}
+			fast++;
 		}
 	}
 }
