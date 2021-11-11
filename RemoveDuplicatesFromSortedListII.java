2743bd93bac6dc3d11e307fcc93c573a15a4704e
powerlich227 <yemingxiao1990@gmail.com>
2021-11-10 22:46:49 -0500

    Medium, LinkedList/TwoPointers

--- RemoveDuplicatesFromSortedListII.java
+++ RemoveDuplicatesFromSortedListII.java
@@ -1,27 +1,45 @@
+/*
+ * LeetCode 82 Medium
+ * 
+ * Remove Duplicates from Sorted List II
+ * 
+ * 
+Given the head of a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list. 
+Return the linked list sorted as well.
 
+Example 1:
+Input: head = [1,2,3,3,4,4,5]
+Output: [1,2,5]
+
+Example 2:
+Input: head = [1,1,1,2,3]
+Output: [2,3]
+ 
+Constraints:
+
+The number of nodes in the list is in the range [0, 300].
+-100 <= Node.val <= 100
+The list is guaranteed to be sorted in ascending order.
+ */
 public class RemoveDuplicatesFromSortedListII {
 	public static void main(String[] args) {
 		System.out.println();
 	}
+	// Two Pointers: pre & cur
 	public ListNode removeDuplicates(ListNode head) {
 		if (head == null || head.next == null)
 			return head;
-		
-		// to avoid the head node is deleted, build a dummy node(-1) as the new head node
-		ListNode dummy = new ListNode(-1); 
-		dummy.next = head; 
-		
+		ListNode dummy = new ListNode(-1);
+		dummy.next = head;
 		ListNode pre = dummy;
 		while (pre.next != null) {
 			ListNode cur = pre.next;
-			while (cur.next != null && cur.val == cur.next.val) {
+			while (cur.next != null && cur.val == cur.next.val)
 				cur = cur.next;
-			}
 			if (pre.next != cur)
 				pre.next = cur.next;
-			else
+			else 
 				pre = pre.next;
-				
 		}
 		return dummy.next;
 	}
