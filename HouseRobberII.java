821a8335cb15172abd5e52d24792b13e6084defb
powerlich227 <yemingxiao1990@gmail.com>
2021-12-04 22:03:33 -0600

    Medium, Array/DP

--- HouseRobberII.java
+++ HouseRobberII.java
@@ -5,11 +5,12 @@
  * 
 You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed. 
 All houses at this place are arranged in a circle. That means the first house is the neighbor of the last one. 
-Meanwhile, adjacent houses have a security system connected, and it will automatically contact the police if two adjacent houses were broken into on the same night.
+Meanwhile, adjacent houses have a security system connected, and it will automatically contact the police 
+if two adjacent houses were broken into on the same night.
 
-Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+Given an integer array nums representing the amount of money of each house, 
+return the maximum amount of money you can rob tonight without alerting the police.
 
- 
 Example 1:
 Input: nums = [2,3,2]
 Output: 3
@@ -25,24 +26,23 @@ Example 3:
 Input: nums = [0]
 Output: 0
 
- 
 Constraints:
 1 <= nums.length <= 100
 0 <= nums[i] <= 1000
  */
 public class HouseRobberII {
-
+	// max(nums[0...n-2], nums[1...n-1])
 	public int rob(int[] nums) {
 		int n = nums.length;
 		if (n == 1)
 			return nums[0];
-		return Math.max(robRange(nums, 0, n - 1), robRange(nums, 1, n));
+		return Math.max(robRange(nums, 0, n - 2), robRange(nums, 1, n - 1));
 	}
 	// dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i])
 	public int robRange(int[] nums, int start, int end) {
 		int dp_i_2 = 0, dp_i_1 = 0, dp_i = 0;
 		
-		for (int i = start; i < end; i++) {
+		for (int i = start; i <= end; i++) {
 			dp_i = Math.max(dp_i_2 + nums[i], dp_i_1);
 			dp_i_2 = dp_i_1;
 			dp_i_1 = dp_i;
