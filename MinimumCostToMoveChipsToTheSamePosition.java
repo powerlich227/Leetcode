50c30c26206321b7f6a9f741f744ef05c0be5b95
powerlich227 <yemingxiao1990@gmail.com>
2021-12-05 23:30:08 -0600

    Easy, Array/Greedy

---
+++ MinimumCostToMoveChipsToTheSamePosition.java
@@ -0,0 +1,53 @@
+/*
+ * LeetCode 1217 Easy
+ * 
+ * Minimum Cost to Move Chips to The Same Position
+ * 
+We have n chips, where the position of the ith chip is position[i].
+We need to move all the chips to the same position. In one step, we can change the position of the ith chip from position[i] to:
+position[i] + 2 or position[i] - 2 with cost = 0.
+position[i] + 1 or position[i] - 1 with cost = 1.
+Return the minimum cost needed to move all the chips to the same position.
+
+Example 1:
+Input: position = [1,2,3]
+Output: 1
+Explanation: First step: Move the chip at position 3 to position 1 with cost = 0.
+Second step: Move the chip at position 2 to position 1 with cost = 1.
+Total cost is 1.
+
+Example 2:
+Input: position = [2,2,2,3,3]
+Output: 2
+Explanation: We can move the two chips at position 3 to position 2. Each move has cost = 1. The total cost = 2.
+
+Example 3:
+Input: position = [1,1000000000]
+Output: 1
+ 
+Constraints:
+1 <= position.length <= 100
+1 <= position[i] <= 10^9
+ */
+public class MinimumCostToMoveChipsToTheSamePosition {
+	// greedy
+	// move to position[i]
+	// even to even / odd to odd: cost=0;
+	// odd to even: cost=1;
+	public int minCostToMoveChips(int[] position) {
+		int odd = 0, even = 0;
+		for (int i : position) {
+			if (i % 2 == 1)
+				odd++;
+			else if (i % 2 == 0)
+				even++;
+		}
+		return Math.min(odd, even);
+	}
+	public static void main(String[] args) {
+		MinimumCostToMoveChipsToTheSamePosition test = new MinimumCostToMoveChipsToTheSamePosition();
+		System.out.println(test.minCostToMoveChips(new int[] {1, 2, 3}));
+		System.out.println(test.minCostToMoveChips(new int[] {2, 2, 2, 3, 3}));
+		System.out.println(test.minCostToMoveChips(new int[] {1, 1000000000}));
+	}
+}
