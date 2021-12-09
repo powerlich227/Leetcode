9ddce39b0a5806c1ed373bf9a99c0b093d9324a9
powerlich227 <yemingxiao1990@gmail.com>
2021-12-08 22:10:43 -0600

    Medium, Array/BFS

--- JumpGameIII.java
+++ JumpGameIII.java
@@ -2,10 +2,12 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 /*
- * LeetCode 1306 Jump Game III
+ * LeetCode 1306 Medium
  * 
- * Given an array of non-negative integers arr, you are initially positioned at start index of the array. 
- * When you are at index i, you can jump to i + arr[i] or i - arr[i], check if you can reach to any index with value 0.
+ * Jump Game III
+ * 
+Given an array of non-negative integers arr, you are initially positioned at start index of the array. 
+When you are at index i, you can jump to i + arr[i] or i - arr[i], check if you can reach to any index with value 0.
 
 Notice that you can not jump outside of the array at any time.
 
