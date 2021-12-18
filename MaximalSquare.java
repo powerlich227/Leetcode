78ae7492f3dbb1bb29cadee5a36d805c167dbd89
powerlich227 <yemingxiao1990@gmail.com>
2021-12-17 21:51:19 -0600

    Medium, Matrix/DP

--- MaximalSquare.java
+++ MaximalSquare.java
@@ -1,11 +1,36 @@
+/*
+ * LeetCode 221 Medium
+ * 
+ * Maximal Square
+ * 
+Given an m x n binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area.
 
+Example 1:
+Input: matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+Output: 4
+
+Example 2:
+Input: matrix = [["0","1"],["1","0"]]
+Output: 1
+
+Example 3:
+Input: matrix = [["0"]]
+Output: 0
+ 
+Constraints:
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 300
+matrix[i][j] is '0' or '1'.
+ */
 public class MaximalSquare {
 	/*
 	 * dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1
 	 */
 	public int maximalSquare(char[][] matrix) {
-		int res = 0, m = matrix.length, n = (m == 0) ? 0 : matrix[0].length;
+		int m = matrix.length, n = matrix[0].length;
 		int[][] dp = new int[m + 1][n + 1];
+		int res = 0;
 		for (int i = 1; i <= m; i++) {
 			for (int j = 1; j <= n; j++) {
 				if (matrix[i - 1][j - 1] == '1') {
