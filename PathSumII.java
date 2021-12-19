d1741089902df5bee44dd5284129100eb0ae851b
powerlich227 <yemingxiao1990@gmail.com>
2021-12-19 17:33:31 -0600

    Medium, BinaryTree/DFS

--- PathSumII.java
+++ PathSumII.java
@@ -1,24 +1,41 @@
 import java.util.*;
+/*
+ * LeetCode 113 Medium
+ * 
+ * Path Sum II
+ * 
+Given the root of a binary tree and an integer targetSum, return all root-to-leaf paths where the sum of the node values in the path equals targetSum. 
+Each path should be returned as a list of the node values, not node references.
 
+A root-to-leaf path is a path starting from the root and ending at any leaf node. A leaf is a node with no children.
+
+Example 1:
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+Output: [[5,4,11,2],[5,8,4,5]]
+Explanation: There are two paths whose sum equals targetSum:
+5 + 4 + 11 + 2 = 22
+5 + 8 + 4 + 5 = 22
+
+Example 2:
+Input: root = [1,2,3], targetSum = 5
+Output: []
+
+Example 3:
+Input: root = [1,2], targetSum = 0
+Output: []
+ */
 public class PathSumII {
 	public static void main(String[] args) {
 		System.out.println();
 	}
+	// DFS
 	public List<List<Integer>> pathSum(TreeNode root, int sum) {
 		List<List<Integer>> res = new ArrayList<>();
 		List<Integer> path = new ArrayList<>();
-		helper(root, sum, path, res);
+		helper(root, sum, res, path);
 		return res;
 	}
-	public void helper(TreeNode root, int sum, List<Integer> path, List<List<Integer>> res) {
-		if (root == null)
-			return;
-		path.add(root.val);
-        List<Integer> out = new ArrayList<>(path);
-		if (root.left == null && root.right == null && root.val == sum)
-			res.add(out);
-		helper(root.left, sum - root.val, path, res);
-		helper(root.right, sum - root.val, path, res);
-		path.remove(path.size() - 1);
+	public void helper(TreeNode root, int sum, List<List<Integer>> res, List<Integer> path) {
+		
 	}
 }
