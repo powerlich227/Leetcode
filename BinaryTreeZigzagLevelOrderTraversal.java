0d2d064fd1213d33e730c9eae4d6ff3a8cab15e3
powerlich227 <yemingxiao1990@gmail.com>
2021-12-19 16:49:24 -0600

    Medium, BinaryTree/BFS/Queue

--- BinaryTreeZigzagLevelOrderTraversal.java
+++ BinaryTreeZigzagLevelOrderTraversal.java
@@ -1,13 +1,40 @@
 import java.util.*;
+
+/*
+ * LeetCode 103 Medium
+ * 
+ * Binary Tree Zigzag Level Order Traversal
+ * 
+Given the root of a binary tree, return the zigzag level order traversal of its nodes' values. 
+(i.e., from left to right, then right to left for the next level and alternate between).
+
+Example 1:
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[20,9],[15,7]]
+
+Example 2:
+Input: root = [1]
+Output: [[1]]
+
+Example 3:
+Input: root = []
+Output: []
+ 
+Constraints:
+The number of nodes in the tree is in the range [0, 2000].
+-100 <= Node.val <= 100
+ */
 public class BinaryTreeZigzagLevelOrderTraversal {
 	public static void main(String[] args) {
 		System.out.println();
 	}
+	// recursion
 	public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
 		List<List<Integer>> res = new ArrayList<>();
 		helper(root, 0, res);
 		return res;
 	}
+	// Recursion/DFS
 	public void helper(TreeNode root, int level, List<List<Integer>> res) {
 		if (root == null)
 			return;
@@ -19,33 +46,35 @@ public class BinaryTreeZigzagLevelOrderTraversal {
 		else
 			res.get(level).add(0, root.val);
 		
-		if (root.left != null)
-			helper(root.left, level + 1, res);
-		if (root.right != null)
-			helper(root.right, level + 1, res);		
+		helper(root.left, level + 1, res);
+		helper(root.right, level + 1, res);		
 	}
-	public List<List<Integer>> zigzagLevelOrder2(TreeNode root) {
-		List<List<Integer>> res = new ArrayList<>();
-		Queue<TreeNode> queue = new LinkedList<>();
-		queue.offer(root);
-		int cnt = 0;
-		while (!queue.isEmpty()) {
-			List<Integer> out = new ArrayList<>();
-			for (int i = queue.size(); i > 0; i--) {
-				TreeNode cur = queue.poll();
-				if (cnt % 2 == 0)
+	// BFS: queue
+	public List<List<Integer>> zigzagLevelOrder2(TreeNode root) {			
+		List<List<Integer>> res = new LinkedList<>();
+		if (root == null)
+			return res;
+		Queue<TreeNode> q = new LinkedList<>();
+		q.offer(root);
+		Boolean flag = true;
+		
+		while (!q.isEmpty()) {
+			LinkedList<Integer> out = new LinkedList<>();
+			int size = q.size();
+			for (int i = 0; i < size; i++) {
+				TreeNode cur = q.poll();
+				if (flag)
 					out.add(cur.val);
 				else
-					out.add(0, cur.val);
+					out.addFirst(cur.val);
 				if (cur.left != null)
-					queue.offer(cur.left);
+					q.offer(cur.left);
 				if (cur.right != null)
-					queue.offer(cur.right);
+					q.offer(cur.right);
 			}
-			cnt++;
+			flag = !flag;
 			res.add(out);
-		}	
+		}
 		return res;
 	}
-
 }
