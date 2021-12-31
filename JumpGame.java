92491adde89c93995028b12237b8b9dae7e39ab1
powerlich227 <yemingxiao1990@gmail.com>
2021-12-30 23:26:23 -0600

    Medium, Array/Greedy

--- JumpGame.java
+++ JumpGame.java
@@ -1,26 +1,41 @@
+/*
+ * LeetCode 55 Medium
+ * 
+ * Jump Game
+ * 
+Given an array of non-negative integers nums, you are initially positioned at the first index of the array.
+Each element in the array represents your maximum jump length at that position.
+Determine if you are able to reach the last index.
 
+Example 1:
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+Example 2:
+Input: nums = [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+ 
+
+Constraints:
+1 <= nums.length <= 3 * 10^4
+0 <= nums[i] <= 10^5
+ */
 public class JumpGame {
 	public static void main(String[] args) {
-		System.out.println();
+		JumpGame test = new JumpGame();
+		System.out.println(test.canJump(new int[] {2, 3, 1, 1, 4}));
+		System.out.println(test.canJump(new int[] {3, 2, 1, 0, 4}));
 	}
-	//Greedy
+	// Greedy
 	public boolean canJump(int[] nums) {
-		int reach = 0;
-		for (int i = 0; i < nums.length; i++) {
-			if (reach < i || reach >= nums.length - 1)
-				break;
+		int reach = 0, n = nums.length;
+		for (int i = 0; i < n - 1; i++) {
 			reach = Math.max(reach, i + nums[i]);
+			if (reach <= i)
+				return false;
 		}
-		return (reach >= nums.length - 1);
+		return true;
 	}
-	//Greedy2
-	public boolean canJump2(int[] nums) {
-		int index = nums.length - 1;
-		for (int i = index; i >= 0; i--) {
-			if (i + nums[i] > index)
-				index = i;
-		}
-		return index == 0;
-	}
-
 }
