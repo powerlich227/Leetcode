ec500c3a512fdeb52c8e6584290ea02527386974
powerlich227 <yemingxiao1990@gmail.com>
2022-01-02 22:48:39 -0600

    Easy, Array/Graph

--- FindTheTownJudge.java
+++ FindTheTownJudge.java
@@ -1,19 +1,55 @@
+/*
+ * LeetCode 997 Easy
+ * 
+ * Find the Town Judge
+ * 
+In a town, there are n people labeled from 1 to n. There is a rumor that one of these people is secretly the town judge.
+If the town judge exists, then:
 
+The town judge trusts nobody.
+Everybody (except for the town judge) trusts the town judge.
+There is exactly one person that satisfies properties 1 and 2.
+You are given an array trust where trust[i] = [ai, bi] representing that the person labeled ai trusts the person labeled bi.
+
+Return the label of the town judge if the town judge exists and can be identified, or return -1 otherwise.
+
+Example 1:
+Input: n = 2, trust = [[1,2]]
+Output: 2
+
+Example 2:
+Input: n = 3, trust = [[1,3],[2,3]]
+Output: 3
+
+Example 3:
+Input: n = 3, trust = [[1,3],[2,3],[3,1]]
+Output: -1
+ 
+Constraints:
+1 <= n <= 1000
+0 <= trust.length <= 10^4
+trust[i].length == 2
+All the pairs of trust are unique.
+ */
 public class FindTheTownJudge {
 	public static void main(String args[]) {
-		System.out.println();
+		FindTheTownJudge test = new FindTheTownJudge();
+		System.out.println(test.findJudge(2, new int[][] {{1,2}}));
+		System.out.println(test.findJudge(3, new int[][] {{1,3},{2,3}}));
+		System.out.println(test.findJudge(3, new int[][] {{1,2},{2,3},{3,1}}));
 	}
-	public int findJudge(int N, int[][]trust) {
-		int[] inDegree = new int[N + 1];
-		int[] outDegree = new int[N + 1];
-		for (int[] t: trust) {
-			outDegree[t[0]]++;
-			inDegree[t[1]]++;
-		}
-		for (int i = 1; i <= N; i++) {
-			if (outDegree[i] == 0 && inDegree[i] == N - 1)
-				return i;
-		}
-		return -1;
-	}
+    // Graph
+    public int findJudge(int n, int[][] trust) {
+        int[] in = new int[n + 1];
+        int[] out = new int[n + 1];
+        for (int[] t : trust) {
+            in[t[1]]++;
+            out[t[0]]++;
+        }
+        for (int i = 1; i <= n; i++) {
+            if (in[i] == n - 1 && out[i] == 0)
+                return i;
+        }
+        return -1;
+    }
 }
