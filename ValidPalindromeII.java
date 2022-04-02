ad1977ef9740c343df513a6f37f9a986b24d5f6d
powerlich227 <yemingxiao1990@gmail.com>
2022-04-02 16:03:33 -0500

    Easy, String/TwoPointers

--- ValidPalindromeII.java
+++ ValidPalindromeII.java
@@ -1,5 +1,29 @@
+/*
+ * LeetCode 680 Easy
+ * 
+ * Valid Palindrome II
+ * 
+ * Given a string s, return true if the s can be palindrome after deleting at most one character from it.
 
+Example 1:
+Input: s = "aba"
+Output: true
+
+Example 2:
+Input: s = "abca"
+Output: true
+Explanation: You could delete the character 'c'.
+
+Example 3:
+Input: s = "abc"
+Output: false
+ 
+Constraints:
+1 <= s.length <= 10^5
+s consists of lowercase English letters.
+ */
 public class ValidPalindromeII {
+	// Two Pointers
 	public boolean validPalindrome(String s) {
 		int l = 0, r = s.length() - 1;
 		while (l < r) {
@@ -10,6 +34,7 @@ public class ValidPalindromeII {
 		}
 		return true;
 	}
+	// Valid Palindrome
 	public boolean helper(String s, int l, int r) {
 		while (l < r) {
 			if (s.charAt(l) != s.charAt(r))
