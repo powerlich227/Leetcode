b43d2da23560f35b56304146a248a6ecc8da3c5b
powerlich227 <yemingxiao1990@gmail.com>
2022-04-06 06:24:28 -0500

    Medium, TwoPointers

---
+++ SwappingNodesInALinkedList.java
@@ -0,0 +1,42 @@
+/*
+ * LeetCode 1721 Medium
+ * 
+ * Swapping Nodes In a Linked List
+ * 
+You are given the head of a linked list, and an integer k.
+
+Return the head of the linked list after swapping the values of the kth node from the beginning and the kth node from the end (the list is 1-indexed).
+
+Example 1:
+Input: head = [1,2,3,4,5], k = 2
+Output: [1,4,3,2,5]
+
+Example 2:
+Input: head = [7,9,6,6,7,8,3,0,9,5], k = 5
+Output: [7,9,6,6,8,7,3,0,9,5]
+ 
+Constraints:
+The number of nodes in the list is n.
+1 <= k <= n <= 10^5
+0 <= Node.val <= 100 
+ */
+public class SwappingNodesInALinkedList {
+    public ListNode swapNodes(ListNode head, int k) {
+        ListNode slow = head, fast = head;
+        for (int i = 1; i < k; i++)
+            fast = fast.next;
+        ListNode first = fast;
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        ListNode second = slow;
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+        return head;
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+}
