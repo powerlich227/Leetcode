e8b3b86bab96803b5dfad4a5d16d4458ce47382e
powerlich227 <yemingxiao1990@gmail.com>
2022-07-23 23:20:45 -0500

    Medium, Array/Matrix/BinarySearch

--- Search2DMatrixII.java
+++ Search2DMatrixII.java
@@ -1,21 +1,51 @@
+/*
+ * LeetCode 240 Medium
+ * 
+ * Search a 2D Matrix II
+ * 
+Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. 
+This matrix has the following properties:
 
+Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
+ 
+
+Example 1:
+Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5
+Output: true
+
+Example 2:
+Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 20
+Output: false
+ 
+
+Constraints:
+m == matrix.length
+n == matrix[i].length
+1 <= n, m <= 300
+-10^9 <= matrix[i][j] <= 10^9
+All the integers in each row are sorted in ascending order.
+All the integers in each column are sorted in ascending order.
+-10^9 <= target <= 10^9
+ */
 public class Search2DMatrixII {
 	public static void main(String[] args) {
-		System.out.println();
+		Search2DMatrixII test = new Search2DMatrixII();
+		int[][] matrix = new int[][] {{1,4,7,11,15}, {2,5,8,12,19}, {3,6,9,16,22}, {10,13,14,17,24}, {18,21,23,26,30}};
+		System.out.println(test.searchMatrix(matrix, 5));
+		System.out.println(test.searchMatrix(matrix, 20));
 	}
 	public boolean searchMatrix(int[][] matrix, int target) {
-		if (matrix.length == 0 || matrix[0].length == 0)
-			return false;
-		int m = matrix.length, n = 0;
-		while (m >= 0 && n < matrix[0].length) {
-			if (matrix[m][n] == target)
+		int m = matrix.length, n = matrix[0].length;
+		int i = 0, j = n - 1;
+		while (i < n && j >= 0) {
+			if (matrix[i][j] == target)
 				return true;
-			if (matrix[m][n] < target)
-				n++;
+			else if (matrix[i][j] < target)
+				i++;
 			else
-				m--;
+				j--;
 		}
 		return false;
 	}
-
 }
